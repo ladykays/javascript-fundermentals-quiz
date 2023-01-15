ee94b4c2894118b93efb1951fc5eccd34aabce27
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-15 03:28:39 +0000

    Add more questions to the question pool

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # javascript-fundermentals-quiz
+
+
+## Credits
+* https://www.youtube.com/watch?v=riDzcEQbX6k&t=27s
+* https://www.youtube.com/watch?v=LQGTb112N_c
+* https://www.youtube.com/watch?v=Tf9hr4tmhK4
+* https://www.sitepoint.com/simple-javascript-quiz/
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,36 @@
 // Question pool for the quiz game
 var questions = [
   {
+    question: "Which of the following is not javascript data type?",
+    choices: ["Null", "Undefined", "Number", "All of the above",],
+    answer: "All of the above",
+  },
+  {
+    question: " What is JavaScript?",
+    choices: ["JavaScript is a scripting language used to make the websites interactive", "JavaScript is an assembly language used to make websites interactive", "JavaScript is an assembly language used to make websites interactive", "None of the above",],
+    answer: "JavaScript is a scripting language used to make the website interactive",
+  },
+  {
+    question: "Which of the following is correct about JavaScript?",
+    choices: ["JavaScript is an Object-Based language", "JavaScript is an Assembly-language", "JavaScript is an Object-Oriented language", "JavaScript is a High-level language",],
+    answer: "JavaScript is an Object-Based language",
+  },
+  {
+    question: " Which of the following is not a framework?",
+    choices: [" JavaScript .NET", "JavaScript", "Cocoa JS", "jQuery",],
+    answer: " JavaScript",
+  },
+  {
+    question: "Which of the following is the property that is triggered in response to JS errors?",
+    choices: ["onClick", "onError", "onMessage", "onException",],
+    answer: "onError",
+  },
+  {
+    question: "Which of the following is not an error in JavaScript?",
+    choices: ["Missing of semicolons", "Missing of Bracket", "Division by zero", "Syntax error",],
+    answer: "Division by zero",
+  },
+  {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["<scripting>", "<javascript>", "<js>", "<script>",],
     answer: "<script>",
@@ -27,7 +57,6 @@ var questions = [
   },
 ];
 
-//Stores how many questions the user got right
-var score = 0;
+
 
 
